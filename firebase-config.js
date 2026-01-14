bd41e61c6bd2e40f86ffe3069cec99f7b0f557e9
rjebhaoui-lgtm <rjebhaoui@gmail.com>
2026-01-15 00:41:14 +0100

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-//
-const firebaseConfig = {
-    apiKey: "AIzaSyC_wp894wyBJisoOkmotBtnzqy44xzP3Gg",
-    authDomain: "esati-7c083.firebaseapp.com",
-    projectId: "esati-7c083",
-    storageBucket: "esati-7c083.firebasestorage.app",
-    messagingSenderId: "432835304984",
-    appId: "1:432835304984:web:3f87364e22e3f362bfad6d"
-};
